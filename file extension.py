c8ee514feb1752bf241c812bf9f11ab96911d5c1
hkedariya <84200659+hkedariya@users.noreply.github.com>
2021-05-27 15:35:41 +0530

    Add files via upload

---
+++ file extension.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+fileName = input("write the name of the file ")
+file_extns= fileName.split(".")
+print(f"The extension of file is :{file_extns[-1]}")
+
+
+# In[10]:
+
+
+f = input("Write the name of the file ")
+ext = f.split(".")[-1]
+d = {"py":"python"}
+A=(d[ext])
+print(f" The extension of file is:{A}")
+input()
+
+
+# In[ ]:
+
+
+
+
---
+++ radius.py
@@ -0,0 +1,5 @@
+Radius= float(input("Enter the radius of the circle:"))
+Area = 3.14*Radius*Radius
+print(Area)
+print(f"The area of the circle with radius {Radius} is: {Area}")
+input()
