06ae4c429603be2b7db6864d87173eff70a3d3f0
hkedariya <84200659+hkedariya@users.noreply.github.com>
2021-05-28 19:43:07 +0530

    Add files via upload

---
+++ mississipi.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+x = "mississipi"
+
+print(x)
+# In[13]:
+
+
+i=x.count("i")
+print(f"i:{i}")
+
+
+# In[15]:
+
+
+s=x.count("s")
+print(f"s:{s}")
+
+
+# In[16]:
+
+
+m=x.count("m")
+print(f"m:{m}")
+
+
+# In[19]:
+
+
+p=x.count("p")
+print(f"p:{p}")
+
+input()
+# In[ ]:
+
+
+
+
+
